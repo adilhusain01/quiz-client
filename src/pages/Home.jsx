5be9edabad599629d33cdba9b8373ed1fcd12b86
Adil husain <husainadil202@gmail.com>
2024-10-08 17:56:17 +0000

    latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,9 +55,15 @@ const Home = () => {
   return (
     <>
       <section 
-        className="w-full grid grid-cols-2"
+        className="mt-[-5rem] flex flex-col items-start justify-center gap-[5rem]"
         style={{ height: 'calc(100vh - 5rem)' }}
       >
+        <span>
+        <h2 className='px-[20rem] text-[7.5rem] font-bold text-white'>Vibe with<span className='mx-[2rem] text-purple-600'>{'Web <3!'}</span></h2>
+
+        </span>
+       
+        <section className='h-full max-h-fit w-full grid grid-cols-2'>
         <div className='flex flex-col items-center justify-center gap-[2.5rem]'>
           <div className="p-[1rem] flex flex-col items-center justify-center bg-white gap-[0.5rem] w-[20rem] rounded-md shadow-md">
             <input 
@@ -65,43 +71,33 @@ const Home = () => {
               value={joinQuizCode} 
               onChange={(e) => setJoinQuizCode(e.target.value)} 
               placeholder="Enter Code" 
-              className="px-[0.5rem] py-[0.25rem] text-[1.1rem] text-center text-black border border-black focus:outline-none w-full rounded-md" 
+              className="px-[0.5rem] py-[1.5rem] text-[1.1rem] text-center text-black border border-black focus:outline-none w-full rounded-md" 
             />
             <button 
               onClick={handleJoinQuiz} 
-              className="px-[0.5rem] py-[0.5rem] text-[1.1rem] text-white text-center bg-matte-dark hover:bg-matte-light w-full rounded-md"
+              className="px-[0.5rem] py-[1rem] text-[1.1rem] text-white text-center bg-matte-dark hover:bg-matte-light w-full rounded-md"
             >
               Join Quiz
             </button>
-          </div>
-          <h2 className='text-[1.5rem] text-white font-bold'>Or</h2>
-          <div className="p-[1rem] flex flex-col items-center justify-center bg-white gap-[0.5rem] w-[20rem] rounded-md shadow-md">
-            <input 
-              type="text" 
-              value={leaderboardsCode} 
-              onChange={(e) => setLeaderboardsCode(e.target.value)} 
-              placeholder="Enter Code" 
-              className="px-[0.5rem] py-[0.25rem] text-[1.1rem] text-center text-black border border-black focus:outline-none w-full rounded-md" 
-            />
-            <Link to={`/leaderboards/${leaderboardsCode}`} className="px-[0.5rem] py-[0.5rem] text-[1.1rem] text-white text-center bg-matte-dark hover:bg-matte-light w-full rounded-md">Leaderboards</Link>
           </div>
         </div>
        
         <div ref={dropdownRef} className="m-auto p-[1rem] flex flex-col items-center justify-center bg-white gap-[0.5rem] w-[20rem] rounded-md shadow-md relative">
           <button 
             onClick={toggleDropdown} 
-            className="px-[0.5rem] py-[0.5rem] text-[1.1rem] text-white bg-matte-dark hover:bg-matte-light w-full rounded-md"
+            className="px-[0.5rem] py-[1rem] text-[1.1rem] text-white bg-matte-dark hover:bg-matte-light w-full rounded-md"
           >
             Create a Quiz
             <ArrowDropDownIcon />
           </button>
           {dropdownOpen && (
             <div className="absolute top-full mt-2 w-full bg-white rounded-md shadow-md">
-              <Link to={'/pdfToQuiz'} className="block px-[0.5rem] py-[0.5rem] text-[1.1rem] text-black hover:bg-gray-200 w-full rounded-md cursor-pointer">Pdf to Quiz</Link>
-              <Link to={'/promptToQuiz'} className="block px-[0.5rem] py-[0.5rem] text-[1.1rem] text-black hover:bg-gray-200 w-full rounded-md cursor-pointer">Prompt to Quiz</Link>
+              <Link to={'/pdfToQuiz'} className="block p-[1rem] text-[1.1rem] text-black hover:bg-gray-200 w-full rounded-md cursor-pointer">Pdf to Quiz</Link>
+              <Link to={'/promptToQuiz'} className="block p-[1rem] text-[1.1rem] text-black hover:bg-gray-200 w-full rounded-md cursor-pointer">Prompt to Quiz</Link>
             </div>
           )}
         </div>
+      </section>
       </section>
     </>
   );
