ecc93f73a1d5dc9f314d13c24475932062355d1d
Adil husain <husainadil202@gmail.com>
2024-11-28 00:16:07 +0530

    .

--- src/pages/QuizOptions.jsx
+++ src/pages/QuizOptions.jsx
@@ -31,7 +31,7 @@ const QuizOptions = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-20">
+    <div className="container mx-auto px-4 py-16">
       <h1 className="text-4xl font-bold text-white text-center mb-12">
         Quiz Options
       </h1>
